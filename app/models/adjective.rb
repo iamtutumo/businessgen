8d106af7f3e303d89bbe7c04d4bb6c901f7a8c93
Isaac Weinbach <isaacito@aol.com>
2015-03-31 18:18:34 -0400

    more in array

--- app/models/adjective.rb
+++ app/models/adjective.rb
@@ -3,7 +3,7 @@ class Adjective < ActiveRecord::Base
 
   
   def initialize
-    a = ["cats", "dogs", "people who are gluten free", "hipsters", "stangers", "italians", "construction workers", "dolphins", "mascots", "putos", "people named Jake", "bread", "spelling bee champions", "kanye west", "programmers"]
+    a = ["cats", "dogs", "people who are gluten free", "hipsters", "italians", "construction workers", "dolphins", "putos", "people named Jake", "bread", "spelling bee champions", "kanye west", "programmers", "transgenders", "Ben Affleck", "midgets", "unicorns", "50 cent", "ninjas", "DJ's", "drug addicts"]
     @adjective = a
   end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -3,7 +3,7 @@ class Business < ActiveRecord::Base
 attr_accessor :business
 
 def initialize
-  b = ["netflix", "tinder", "uber", "airline", "cruise", "stock market", "chipotle", "apple store", "museum", "instagram", "college", "art basel", "pajamas", "mermelada", "chicken", "waffles", "cellphones", "speaker sytem", "towles", "computer", "porn website", "iphone app", "buffet", "jeans", "mints", "sauce", "massages", "cake", "office supplies"]
+  b = ["socks","glitter","netflix", "tinder", "uber", "airline", "cruise", "stock market", "chipotle", "apple store", "museum", "instagram", "college", "art basel", "pajamas", "mermelada", "chicken", "waffles", "cellphones", "speaker sytem", "towels", "computer", "porn website", "iphone app", "buffet", "jeans", "mints", "sauce", "massages", "cake", "office supplies", "pizza", "bouncy ball", "circus", "milk", "pillow", "hair"]
   @business = b
 end
 
