d4f95588a7e3855aba131de48a838c34f4c8c5bf
Isaac Weinbach <isaacito@aol.com>
2015-03-31 16:10:36 -0400

    responsive

--- app/models/adjective.rb
+++ app/models/adjective.rb
@@ -3,7 +3,7 @@ class Adjective < ActiveRecord::Base
 
   
   def initialize
-    a = ["cats", "dogs", "people who are gluten free", "hipsters", "stangers", "italians", "construction workers", "dolphins", "mascots", "putos"]
+    a = ["cats", "dogs", "people who are gluten free", "hipsters", "stangers", "italians", "construction workers", "dolphins", "mascots", "putos", "people named Jake", "bread", "spelling bee champions", "kanye west", "programmers"]
     @adjective = a
   end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -3,8 +3,8 @@ class Business < ActiveRecord::Base
 attr_accessor :business
 
 def initialize
-  b = ["netflix", "tinder", "uber", "airline", "cruise", "stock market", "chipotle", "apple store", "museum", "instagram", "college", "art basel"]
-  @business =b
+  b = ["netflix", "tinder", "uber", "airline", "cruise", "stock market", "chipotle", "apple store", "museum", "instagram", "college", "art basel", "pajamas", "mermelada", "chicken", "waffles", "cellphones", "speaker sytem", "towles", "computer", "porn website", "iphone app", "buffet", "jeans", "mints", "sauce", "massages", "cake", "office supplies"]
+  @business = b
 end
 
 end
