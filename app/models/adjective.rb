7bb716f599fc9692223911c3b0f00cfd079c6db1
Isaac Weinbach <isaacito@aol.com>
2015-04-01 19:59:50 -0400

    parrot

--- app/models/adjective.rb
+++ app/models/adjective.rb
@@ -3,7 +3,7 @@ class Adjective < ActiveRecord::Base
 
   
   def initialize
-    a = ["cats", "dogs", "people who are gluten free", "hipsters", "italians", "construction workers", "dolphins", "putos", "people named Jake", "bread", "spelling bee champions", "kanye west", "programmers", "transgenders", "Ben Affleck", "midgets", "unicorns", "50 cent", "ninjas", "DJ's", "drug addicts", "alcoholics", "pre-schoolers", "shmohawks", "mexicans", "isis", "hamburgers", "aunt jemaima", "cream cheese", "pajaros", "people named miguel", "teddy bears", "dingle berries", "rabbis", "uber drivers", "grandparents", "yogis", "people that love granola", "kesha", "jj ice fish", "terio", "sluts"]
+    a = ["cats", "dogs", "people who are gluten free", "hipsters", "italians", "construction workers", "dolphins", "putos", "people named Jake", "bread", "spelling bee champions", "kanye west", "programmers", "transgenders", "Ben Affleck", "midgets", "unicorns", "50 cent", "ninjas", "DJ's", "drug addicts", "alcoholics", "pre-schoolers", "shmohawks", "mexicans", "isis", "hamburgers", "aunt jemaima", "cream cheese", "pajaros", "people named miguel", "teddy bears", "dingle berries", "rabbis", "uber drivers", "grandparents", "yogis", "people that love granola", "kesha", "jj ice fish", "terio", "sluts", "Barack Obama", "martha stewart", "glass blowers", "girls that say OMG", "emo people","fred flintstone", "donald trump", "frat bros", "smelly people", "starbucks baristas", "carl", "housekeepers"]
     @adjective = a
   end
 end
