17d4cba30e3a176e238027496ebd04c7bca914fd
Isaac Weinbach <isaacito@aol.com>
2015-04-01 18:15:09 -0400

    array

--- app/models/adjective.rb
+++ app/models/adjective.rb
@@ -3,7 +3,7 @@ class Adjective < ActiveRecord::Base
 
   
   def initialize
-    a = ["cats", "dogs", "people who are gluten free", "hipsters", "italians", "construction workers", "dolphins", "putos", "people named Jake", "bread", "spelling bee champions", "kanye west", "programmers", "transgenders", "Ben Affleck", "midgets", "unicorns", "50 cent", "ninjas", "DJ's", "drug addicts", "alcoholics", "pre-schoolers", "shmohawks", "mexicans", "isis", "hamburgers", "aunt jemaima", "cream cheese", "pajaros"]
+    a = ["cats", "dogs", "people who are gluten free", "hipsters", "italians", "construction workers", "dolphins", "putos", "people named Jake", "bread", "spelling bee champions", "kanye west", "programmers", "transgenders", "Ben Affleck", "midgets", "unicorns", "50 cent", "ninjas", "DJ's", "drug addicts", "alcoholics", "pre-schoolers", "shmohawks", "mexicans", "isis", "hamburgers", "aunt jemaima", "cream cheese", "pajaros", "people named miguel", "teddy bears", "dingle berries", "rabbis", "uber drivers", "grandparents", "yogis", "people that love granola", "kesha", "jj ice fish", "terio", "sluts"]
     @adjective = a
   end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -3,7 +3,7 @@ class Business < ActiveRecord::Base
 attr_accessor :business
 
 def initialize
-  b = ["socks","glitter","netflix", "tinder", "uber", "airline", "cruise", "stock market", "chipotle", "apple store", "museum", "instagram", "college", "art basel", "pajamas", "mermelada", "chicken", "waffles", "cellphones", "speaker sytem", "towels", "computer", "porn website", "iphone app", "buffet", "jeans", "mints", "sauce", "massages", "cake", "office supplies", "pizza", "bouncy ball", "circus", "milk", "pillow", "hair", "designer shoes", "apartments", "google", "aquariums", "fanny packs", "ebay", "amazon", "air bnb", "coupons", "sun glasses"]
+  b = ["socks","glitter","netflix", "tinder", "uber", "airline", "cruise", "stock market", "chipotle", "apple store", "museum", "instagram", "college", "art basel", "pajamas", "mermelada", "chicken", "waffles", "cellphones", "speaker system", "towels", "computer", "porn website", "iphone app", "buffet", "jeans", "mints", "sauce", "massages", "cake", "office supplies", "pizza", "bouncy ball", "circus", "milk", "pillow", "hair", "designer shoes", "apartments", "google", "aquariums", "fanny packs", "ebay", "amazon", "air bnb", "coupons", "sun glasses", "tacos", "ice cream", "chandeliers", "protien shakes", "karate", "sun screen", "hooka"]
   @business = b
 end
 
