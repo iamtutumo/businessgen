656a74568a57a6ee9c8b1d270514e897ee733cde
Isaac Weinbach <isaacito@aol.com>
2015-04-01 15:20:34 -0400

    more in array

--- app/models/adjective.rb
+++ app/models/adjective.rb
@@ -3,7 +3,7 @@ class Adjective < ActiveRecord::Base
 
   
   def initialize
-    a = ["cats", "dogs", "people who are gluten free", "hipsters", "italians", "construction workers", "dolphins", "putos", "people named Jake", "bread", "spelling bee champions", "kanye west", "programmers", "transgenders", "Ben Affleck", "midgets", "unicorns", "50 cent", "ninjas", "DJ's", "drug addicts"]
+    a = ["cats", "dogs", "people who are gluten free", "hipsters", "italians", "construction workers", "dolphins", "putos", "people named Jake", "bread", "spelling bee champions", "kanye west", "programmers", "transgenders", "Ben Affleck", "midgets", "unicorns", "50 cent", "ninjas", "DJ's", "drug addicts", "alcoholics", "pre-schoolers", "shmohawks", "mexicans", "isis", "hamburgers", "aunt jemaima", "cream cheese", "pajaros"]
     @adjective = a
   end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -3,7 +3,7 @@ class Business < ActiveRecord::Base
 attr_accessor :business
 
 def initialize
-  b = ["socks","glitter","netflix", "tinder", "uber", "airline", "cruise", "stock market", "chipotle", "apple store", "museum", "instagram", "college", "art basel", "pajamas", "mermelada", "chicken", "waffles", "cellphones", "speaker sytem", "towels", "computer", "porn website", "iphone app", "buffet", "jeans", "mints", "sauce", "massages", "cake", "office supplies", "pizza", "bouncy ball", "circus", "milk", "pillow", "hair"]
+  b = ["socks","glitter","netflix", "tinder", "uber", "airline", "cruise", "stock market", "chipotle", "apple store", "museum", "instagram", "college", "art basel", "pajamas", "mermelada", "chicken", "waffles", "cellphones", "speaker sytem", "towels", "computer", "porn website", "iphone app", "buffet", "jeans", "mints", "sauce", "massages", "cake", "office supplies", "pizza", "bouncy ball", "circus", "milk", "pillow", "hair", "designer shoes", "apartments", "google", "aquariums", "fanny packs", "ebay", "amazon", "air bnb", "coupons", "sun glasses"]
   @business = b
 end
 
